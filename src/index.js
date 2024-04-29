0f0d3ff2c752f3dabb03f70bd9e5c8d98f51992c
Mandill <mandilthapamagar58@gmail.com>
2024-04-29 21:35:42 +0545

    "Database connection"

---
+++ src/index.js
@@ -0,0 +1,27 @@
+/*
+import express from "express"
+const app = express()
+(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("ERROR",error);
+            throw error
+        })
+        app.listen(process.env.PORT,()=>
+        {
+            console.log(`Listening to the port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.log("ERROR :",error);
+    }
+})()
+*/
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:"./env"
+})
+
+connectDB()
