8b074b250e5a8378d6f725d9c096c06aaa028781
Mandill <mandilthapamagar58@gmail.com>
2024-04-30 14:31:36 +0545

    utils

--- src/index.js
+++ src/index.js
@@ -25,3 +25,14 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>
+{
+    app.listen(process.env.PORT || 8000, ()=>
+    {
+        console.log("server running at port ",process.env.PORT);
+    })
+})
+.catch(error =>
+    {
+        console.log("Mongo Db Connection Failed :",error);
+    })
