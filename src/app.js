2c965bd6359c6e690f179197bc6423083912e05e
Mandill <mandilthapamagar58@gmail.com>
2024-05-02 13:02:41 +0545

    mini

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(cors({
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true}))
 app.use(express.static("public"))
-app.use(cookieParser())
+app.use(cookieParser()) //using serving the cookies of the user can be accessed and set,basically CRUD operation 
 
 
 
--- src/db/index.js
+++ src/db/index.js
@@ -1,29 +1,3 @@
-// import mongoose from "mongoose";
-// import express from "express"
-// import { DB_NAME } from "../constants.js";
-
-// const app = express()
-// const connectDB = async() =>
-// {
-//     try {
-//         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-//         console.log(`\n MongoDB connected !! DB Host : ${connectionInstance.connection.host}`);
-//         // app.on("error",(error)=>
-//         // {
-//         //     console.log("Error :",error);
-//         // })
-//         // app.listen(process.env.PORT,()=>
-//         // {
-//         //     console.log("server running on port ",process.env.PORT);
-//         // })
-//     } catch (error) {
-//         console.log("Error :",error);
-//         throw error;
-//     }
-// }
-
-// export default connectDB
-
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
@@ -35,7 +9,6 @@ const connectDB = async () =>
     } catch (error) {
         console.log("MONGODB Connection Error :",Error);
         throw error;
-        process.exit(1)
     }
 }
 
